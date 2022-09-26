befbbf31c4bd350c3a1f423ad8682ed7fa5e9a98
Satwika7 <113924355+Satwika7@users.noreply.github.com>
2022-09-27 01:37:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -70,9 +70,4 @@ while repeat!= "No":
     #cipher_text = "mjqqt"
     #print output: "The encoded text is mjqqt"
 
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+    
